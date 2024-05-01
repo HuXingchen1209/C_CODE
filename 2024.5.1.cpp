6334dba8a003737b1d9b13ee460d7503fe21ffb1
HuXingchen1209 <2141484722@qq.com>
2024-05-01 22:15:52 +0800

    提交代码

---
+++ 2024.5.1.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	a = 40;
+	b = 20;
+	c = 30;
+	if (a > b)
+		printf("a>b\n");
+	if (a > c)
+		printf("a>c");
+	printf(" ");
+	return 0;
+
+}
---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	a = 3;
+	b = 4;
+	c = 5;
+	printf("%d\t%d\t%d\t", a, b, c);
+	return 0;
+}
